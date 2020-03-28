8fb600500062d886b4e87d5e1df6c07d76936567
irons <irons.du@gmail.com>
2020-03-28 12:06:59 +0800

    refactor interceptor

--- src/gayrpc/core/GayRpcInterceptor.h
+++ src/gayrpc/core/GayRpcInterceptor.h
@@ -12,58 +12,46 @@ namespace gayrpc { namespace core {
     template<typename... Interceptors>
     UnaryServerInterceptor makeInterceptor(Interceptors... interceptors)
     {
-        return [=](RpcMeta&& meta,
-            const google::protobuf::Message& message,
-            UnaryHandler&& tail,
-            InterceptorContextType&& context) {
+        std::vector<UnaryServerInterceptor> userInterceptors{ interceptors... };
 
-                class WrapNextInterceptor final
-                {
-                public:
-                    WrapNextInterceptor(std::vector<UnaryServerInterceptor>&& interceptors,
-                        UnaryHandler&& tail) noexcept
-                        :
-                        mCurIndex(0),
-                        mInterceptors(std::move(interceptors)),
-                        mTail(std::forward<UnaryHandler>(tail))
-                    {
-                    }
-
-                    InterceptorReturnType operator () (RpcMeta&& meta,
-                        const google::protobuf::Message& message,
-                        InterceptorContextType&& context)
-                    {
-                        if (mInterceptors.size() == mCurIndex)
-                        {
-                            return mTail(std::forward<RpcMeta>(meta), 
-                                message, 
-                                std::forward<InterceptorContextType>(context));
-                        }
-                        else
-                        {
-                            mCurIndex++;
-                            auto interceptor = std::move(mInterceptors[mCurIndex - 1]);
-                            auto result = interceptor(std::forward<RpcMeta>(meta),
-                                message, 
-                                std::move(*this), 
-                                std::forward<InterceptorContextType>(context));
-                            // expect the std::move(*this) is really successful;
-                            assert(mTail == nullptr);
-                            assert(mInterceptors.empty());
-                            return result;
-                        }
-                    }
-
-                private:
-                    size_t                              mCurIndex;
-                    std::vector<UnaryServerInterceptor> mInterceptors;
-                    UnaryHandler                        mTail;
-                };
-
-                WrapNextInterceptor next(std::vector<UnaryServerInterceptor>{ interceptors... },
-                    std::forward<UnaryHandler>(tail));
-                return next(std::move(meta), message, std::forward<InterceptorContextType>(context));
+        UnaryServerInterceptor combinationInterceptor = [](
+                gayrpc::core::RpcMeta&& meta,
+                const google::protobuf::Message& message,
+                UnaryHandler&& next,
+                InterceptorContextType&& context)
+        {
+            return next(std::move(meta), message, std::move(context));
         };
+
+        for(auto it = userInterceptors.crbegin(); it != userInterceptors.crend(); it++)
+        {
+            auto wrapper = [userInterceptor = *it](UnaryServerInterceptor nextInterceptor)
+            {
+                return [=](
+                        gayrpc::core::RpcMeta&& meta,
+                        const google::protobuf::Message& message,
+                        UnaryHandler&& next,
+                        InterceptorContextType&& context) mutable
+                {
+                    return userInterceptor(
+                                std::move(meta),
+                                message,
+                                [=, next = std::move(next)](gayrpc::core::RpcMeta&& meta,
+                                    const google::protobuf::Message& message,
+                                    InterceptorContextType&& context) mutable
+                                {
+                                    return nextInterceptor(std::move(meta),
+                                                           message,
+                                                           std::move(next),
+                                                           std::move(context));
+                                },
+                                std::move(context));
+                };
+            };
+            combinationInterceptor = wrapper(combinationInterceptor);
+        }
+
+        return combinationInterceptor;
     }
 
 } }
